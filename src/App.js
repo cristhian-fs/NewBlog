4d47ace8a2976b3d2e6bcc35570f3afe92cc14da
Cristhian <cristhianfernandolp@gmail.com>
2023-10-10 21:05:01 -0300

    Creating the structure of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from './components/Header/index.js'
+import Article from './components/Article/index.js'
 
 function App() {
   return (
-    <h1>Blog</h1>
+    <>
+      <Header></Header>
+      <Article></Article>
+      <h1>Blog</h1>
+    </>
   );
 }
 
---
+++ src/components/Article/index.js
@@ -0,0 +1,13 @@
+import Post from '../Posts/index.js'
+
+const Article= () =>{
+  return (
+    <>
+      <h1>Article.js</h1>
+      <Post />
+    </>
+    
+  )
+}
+
+export default Article
